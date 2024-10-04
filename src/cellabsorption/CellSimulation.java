98ba92f7f49728fc5b6dd00389266c759c194004
shellybeee <117469126+shellybeee@users.noreply.github.com>
2024-10-04 17:22:42 -0500

    updated to make many cells in motion!

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -1,6 +1,8 @@
 package cellabsorption;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import edu.macalester.graphics.CanvasWindow;
@@ -10,17 +12,25 @@ import edu.macalester.graphics.Point;
 public class CellSimulation {
     private CanvasWindow canvas;
     private Random rand = new Random();
-    private Cell cell;
+    private List<Cell> cells;
+
 
     private void populateCells() {
-        double size = rand.nextInt(5) + 2;
-        cell = new Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        canvas.add(cell.getShape());
+        cells = new ArrayList<>();
+
+        for (int i = 0; i <200; i++) {
+            double size = rand.nextInt(5) + 2;
+            Cell cell = new Cell(
+                rand.nextDouble() * (canvas.getWidth() - size),
+                rand.nextDouble() * (canvas.getWidth() - size),
+                size,
+                Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
+            canvas.add(cell.getShape());
+            cells.add(cell);
+        }
     }
+
+
 
     public static void main(String[] args) {
         new CellSimulation();
@@ -31,10 +41,15 @@ public class CellSimulation {
         populateCells();
 
         //noinspection InfiniteLoopStatement
+
+        
         while (true) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
+
+            for (Cell cell : cells) {
             cell.moveAround(canvasCenter);
             cell.grow(0.02);
+            }
 
             canvas.draw();
             canvas.pause(10);
